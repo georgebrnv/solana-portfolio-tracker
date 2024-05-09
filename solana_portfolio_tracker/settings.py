abeebb807a7c2041488fdd5e15e759ed3331bb4f
georgebrnv <barinov.egor2013@gmail.com>
2024-05-09 09:05:19 -0400

    Add domain, turn debug mode off.

--- solana_portfolio_tracker/settings.py
+++ solana_portfolio_tracker/settings.py
@@ -29,9 +29,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SETTINGS_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["0.0.0.0", "127.0.0.1", "seashell-app-aq7tp.ondigitalocean.app"]
+ALLOWED_HOSTS = ["0.0.0.0", "127.0.0.1", "seashell-app-aq7tp.ondigitalocean.app", 'trackoff.app']
 
 
 # Helius credentials
